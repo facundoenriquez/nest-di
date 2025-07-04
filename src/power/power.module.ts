ae6cfdaba7467a31d87fab2762b5b598a72fae04
Facundo Enriquez <fenriquez@inym.org.ar>
2025-07-04 13:36:01 -0300

    import export modules and services

--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  exports: [PowerService], // Exporting PowerService to make it available in other modules
 })
 export class PowerModule {}
